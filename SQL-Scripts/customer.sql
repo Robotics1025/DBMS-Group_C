5779521a32af2763d8a5456699fabb119b0f2349
MUGOLE JOEL <joelmugole74@gmail.com>
2025-10-03 15:59:57 +0300

    updated sys

--- SQL-Scripts/customer.sql
+++ SQL-Scripts/customer.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  Navicat Premium Dump SQL
 
@@ -42,48 +41,3 @@ CREATE TABLE `customer`  (
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
-=======
-/*
- Navicat Premium Dump SQL
-
- Source Server         : Bike-Rental Connection
- Source Server Type    : MySQL
- Source Server Version : 50714 (5.7.14)
- Source Host           : localhost:3306
- Source Schema         : bike-rental
-
- Target Server Type    : MySQL
- Target Server Version : 50714 (5.7.14)
- File Encoding         : 65001
-
- Date: 12/09/2025 11:42:12
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for customer
--- ----------------------------
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE `customer`  (
-  `CustomerID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `NationalID` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `FirstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `LastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `Email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `PhoneNumber` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL,
-  `DateOfBirth` date NULL DEFAULT NULL,
-  `RegistrationDate` datetime NOT NULL,
-  `PasswordHash` char(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  PRIMARY KEY (`CustomerID`) USING BTREE,
-  UNIQUE INDEX `NationalID`(`NationalID`) USING BTREE,
-  UNIQUE INDEX `Email`(`Email`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of customer
--- ----------------------------
-
-SET FOREIGN_KEY_CHECKS = 1;
->>>>>>> 3c66385 (the frontend)
--- SQL-Scripts/feedback.sql
+++ SQL-Scripts/feedback.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  Navicat Premium Dump SQL
 
@@ -33,39 +32,3 @@ CREATE TABLE `feedback`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
-=======
-/*
- Navicat Premium Dump SQL
-
- Source Server         : Bike-Rental Connection
- Source Server Type    : MySQL
- Source Server Version : 50714 (5.7.14)
- Source Host           : localhost:3306
- Source Schema         : bike-rental
-
- Target Server Type    : MySQL
- Target Server Version : 50714 (5.7.14)
- File Encoding         : 65001
-
- Date: 12/09/2025 11:50:03
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for feedback
--- ----------------------------
-DROP TABLE IF EXISTS `feedback`;
-CREATE TABLE `feedback`  (
-  `FeedbackID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `RentalID` int(10) UNSIGNED NOT NULL,
-  `Rating` tinyint(4) NOT NULL,
-  `Comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL,
-  `FeedbackDate` datetime NOT NULL,
-  PRIMARY KEY (`FeedbackID`) USING BTREE,
-  UNIQUE INDEX `RentalID`(`RentalID`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
->>>>>>> 3c66385 (the frontend)
