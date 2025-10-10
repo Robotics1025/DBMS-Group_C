70827b28ce771cc0006ef64d5d0e095a85df0d0a
MUGOLE JOEL <joelmugole74@gmail.com>
2025-10-10 18:41:31 +0300

    the updated thglobal.css

---
+++ bike_rent/global.d.ts
@@ -0,0 +1,48 @@
+// Global TypeScript declarations
+
+// CSS Module declarations
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+// Image file declarations
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
